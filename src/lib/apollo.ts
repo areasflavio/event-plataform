78a923d9de34065695fd7c93487a67dfe1843b93
Flávio Arêas <vflavio9@gmail.com>
2022-06-24 11:22:27 -0300

    feat: create Subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,8 +1,13 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const API_URI = import.meta.env.VITE_API_CONTENT_URL || '';
+const API_CREATE_SUBSCRIBER_TOKEN =
+  import.meta.env.VITE_API_CREATE_SUBSCRIBER_TOKEN || '';
 
 export const client = new ApolloClient({
   uri: API_URI,
+  headers: {
+    Authorization: `Bearer ${API_CREATE_SUBSCRIBER_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
